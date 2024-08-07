99c2ab5433d7866e087703ec3ab986407d483d36
Olawunmi-Afeez-Olamide <64313559+Olawunmi-Afeez-Olamide@users.noreply.github.com>
2024-08-07 15:13:56 +0100

    Create Simple_UART_echo.c
    This is a basic code to establish a relationship between the STM and PUTTY.

---
+++ Simple_UART_echo.c
@@ -0,0 +1,44 @@
+#include "stm32g070xx.h"
+
+void UART2_Init(void);
+void UART2_Transmit(char ch);
+char UART2_Receive(void);
+char ch;
+
+void UART2_Init(void) {
+    // Enable clock for GPIOA and USART2
+    RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+    RCC->APBENR1 |= RCC_APBENR1_USART2EN;
+    
+    // Configure PA2 as USART2_TX and PA3 as USART2_RX
+    GPIOA->MODER &= ~(GPIO_MODER_MODE2 | GPIO_MODER_MODE3);
+    GPIOA->MODER |= (GPIO_MODER_MODE2_1 | GPIO_MODER_MODE3_1); // Set to alternate function mode
+    GPIOA->AFR[0] |= (1 << GPIO_AFRL_AFSEL2_Pos) | (1 << GPIO_AFRL_AFSEL3_Pos); // Set AF1 (USART2) for PA2 and PA3
+    
+    // Configure USART2
+    USART2->BRR = 16000000 / 9600; // Assuming 8 MHz clock, set baud rate to 9600
+    USART2->CR1 |= USART_CR1_TE | USART_CR1_RE; // Enable transmitter and receiver
+    USART2->CR1 |= USART_CR1_UE; // Enable USART2
+}
+
+void UART2_Transmit(char ch) {
+    // Wait until transmit data register is empty
+    while (!(USART2->ISR & USART_ISR_TXE_TXFNF));
+    // Write data to the transmit data register
+    USART2->TDR = ch;
+}
+
+char UART2_Receive(void) {
+    while (!(USART2->ISR & USART_ISR_RXNE_RXFNE)); // Wait until data is received
+    return (char)USART2->RDR;
+}
+
+
+int main(void) {
+    UART2_Init();
+    
+    while (1) {
+        ch = UART2_Receive();
+        UART2_Transmit(ch); // Echo received character
+    }
+}
